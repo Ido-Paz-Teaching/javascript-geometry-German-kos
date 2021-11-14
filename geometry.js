876cbec66bfbcafd1ba8d33eb2901f063dd509d6
German-kos <KostiakovG@gmail.com>
2021-11-14 22:28:07 +0200

    Asignment complete

--- geometry.js
+++ geometry.js
@@ -1,17 +1,16 @@
 function getTriangleArea(height, width) {
   /* Your amazing code here 🚀 */
-   return height*width / 2;
-   
+  return height * width / 2;
 }
 
 function getSquareArea(side) {
   /* Your amazing code here 🚀 */
-return Math.pow(side, 2);
+  return Math.pow(side, 2);
 }
 
 function getCircleArea(radius) {
   /* Your amazing code here 🚀 */
-  return parseFloat((Math.PI*Math.pow(radius,2)).toFixed(2));
+  return parseFloat((Math.PI * Math.pow(radius, 2)).toFixed(2));
 }
 
 //Please , don't remove the following code 
